60168cc52e82dd2768095ecb2f778890dec62806
Brent Matthysen <bmat4469@gmail.com>
2023-08-12 15:51:06 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -8,6 +8,10 @@ import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 
+from sklearn.model_selection import train_test_split
+
+import mlflow
+
 
 # define functions
 def main(args):
